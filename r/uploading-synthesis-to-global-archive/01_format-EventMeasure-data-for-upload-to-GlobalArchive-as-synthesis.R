64dbb46ebd3c80ca2617354b69ad584c37eb3ac7
Abbey Gibbons <abbey.gibbons@uwa.edu.au>
2026-06-02 14:09:21 +0800

    adding metadata & making uploads for global archive

---
+++ r/uploading-synthesis-to-global-archive/01_format-EventMeasure-data-for-upload-to-GlobalArchive-as-synthesis.R
@@ -0,0 +1,156 @@
+# Before you use this script please read the below instructions!!
+# This workflow assumes all of your metadata is correctly formatted, and has been cleaned by using CheckEM. CheckEM available here: https://marine-ecology.shinyapps.io/CheckEM/
+# Please see the CheckEM usermanual for the correct format(https://globalarchivemanual.github.io/CheckEM/articles/manuals/CheckEM_user_guide.html)
+
+# Load libraries -----
+library('remotes')
+options(timeout=9999999)
+# remotes::install_github("GlobalArchiveManual/CheckEM") # Run this if you do not have CheckEM installed.
+library(CheckEM)
+library(tidyverse)
+library(here)
+
+# Set name for synthesis, will be used as prefix for your files to upload
+name <- "geographe-marine-park"
+
+# All data needs to be saved in a folder structure "data/raw/" for the script to work, or change the direcory when reading in files.
+
+# Read in metadata ----
+# Note check if you have used opcode, opcode or period and change the below code accordingly.
+
+
+metadata <- read_metadata(here::here("data/raw/"), method = "BRUVs") %>% # Change here to "DOVs"
+  dplyr::select(campaignid, opcode, 
+                status, 
+                longitude_dd, latitude_dd, 
+                observer_count, observer_length,
+                date_time, 
+                location, site, 
+                depth_m, 
+                successful_count, 
+                successful_length, 
+                successful_habitat_forward, 
+                successful_habitat_backward,
+                observer_habitat_forward,
+                observer_habitat_backward) %>%
+  # rename(opcode = opcode) %>% # use this line if you need to rename opcode to opcode
+  dplyr::filter(campaignid %in% c("2014-12_Geographe-bay_stereo-BRUVs", "2024-04_Geographe_stereo-BRUVs")) %>%
+  glimpse()
+
+duplicates <- metadata %>%
+  dplyr::group_by(campaignid, opcode) %>%
+  dplyr::summarise(n=n()) %>%
+  dplyr::filter(n>1)
+
+unique(metadata$campaignid)
+
+write_csv(metadata, paste0("data/uploads/", name, "_metadata.csv"))
+
+# Read in the maxn and length data ----
+maxn <- read_points(here::here("data/raw/")) %>% 
+  dplyr::mutate(species = ifelse(species %in% c("sp", "sp1", "sp2", "sp10"), "spp", as.character(species))) %>%
+  dplyr::group_by(campaignid, opcode, filename, periodtime, frame, family, genus, species, stage) %>% # If you have MaxN'd by stage (e.g. Adult, Juvenile) add stage here
+  dplyr::mutate(number = as.numeric(number)) %>%
+  dplyr::summarise(maxn = sum(number)) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(campaignid, opcode, family, genus, species, stage) %>%
+  dplyr::slice(which.max(maxn)) %>%
+  dplyr::ungroup() %>%
+  dplyr::filter(!is.na(maxn)) %>%
+  dplyr::select(-frame) %>%
+  tidyr::replace_na(list(maxn = 0)) %>%
+  dplyr::mutate(maxn = as.numeric(maxn)) %>%
+  dplyr::filter(maxn > 0) %>%
+  dplyr::inner_join(metadata, by = join_by(campaignid, opcode)) %>%
+  dplyr::filter(successful_count %in% c("Yes")) %>% 
+  dplyr::filter(maxn > 0) %>%
+  dplyr::select(campaignid, opcode, family, genus, species, maxn, stage) %>%
+  dplyr::mutate(family = ifelse(family %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(family))) %>%
+  dplyr::mutate(genus = ifelse(genus %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(genus))) %>%
+  dplyr::mutate(species = ifelse(species %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "spp", as.character(species))) %>%
+  
+  {message(paste(length(which(.$family %in% "Unknown")), "rows removed because family is 'Unknown'"));
+    .} %>%
+  dplyr::filter(!family %in% "Unknown")
+
+metadata %>%
+  count(campaignid, opcode) %>%
+  filter(n > 1)
+
+metadata %>%
+  count(campaignid, opcode) %>%
+  filter(n > 1)
+
+length <- read_em_length(here::here("data/raw/")) %>%
+  dplyr::mutate(species = ifelse(species %in% c("sp", "sp1", "sp2", "sp10"), "spp", as.character(species))) %>%
+  dplyr::select(-c(comment))%>% # there is a comment column in metadata, so you will need to remove this column from EM data
+  dplyr::inner_join(metadata, by = join_by(opcode, campaignid)) %>%
+  dplyr::filter(successful_length %in% "Yes") %>%
+  dplyr::mutate(family = ifelse(family %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(family))) %>%
+  dplyr::mutate(genus = ifelse(genus %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "Unknown", as.character(genus))) %>%
+  dplyr::mutate(species = ifelse(species %in% c("NA", "NANA", NA, "unknown", "", NULL, " ", NA_character_), "spp", as.character(species)))  %>%
+  
+  {message(paste(length(which(.$family %in% "Unknown")), "rows removed because family is 'Unknown'"));
+    .} %>%
+  dplyr::filter(!family %in% "Unknown")
+
+# Format data for upload to GA and final checks -----
+codes <- australia_life_history %>%
+  dplyr::select(family, genus, species, caab_code)
+
+count_upload <- maxn %>%
+  dplyr::rename(count = maxn) %>%
+  mutate(family = if_else(genus %in% "Anoplocapros", "Aracanidae", family)) %>%
+  mutate(family = if_else(genus %in% "Aracana", "Aracanidae", family)) %>% 
+  mutate(genus = if_else(genus %in% "Dasyatis", "Bathytoshia", genus)) %>%
+  #mutate(genus = if_else(genus %in% "Dasyatis", "Bathytoshia", genus)) %>% #turn this on if you need it 
+  dplyr::left_join(., CheckEM::aus_synonyms) %>%
+  dplyr::mutate(genus = ifelse(!genus_correct%in%c(NA), genus_correct, genus)) %>%
+  dplyr::mutate(species = ifelse(!is.na(species_correct), species_correct, species)) %>%
+  dplyr::mutate(family = ifelse(!is.na(family_correct), family_correct, family)) %>%
+  dplyr::select(-c(family_correct, genus_correct, species_correct)) %>%
+  dplyr::mutate(scientific = paste(family, genus, species)) %>%
+  #filter(!is.na(caab_code))%>%
+  left_join(codes) %>%
+  glimpse()
+
+synonyms_in_count <- dplyr::left_join(count_upload, CheckEM::aus_synonyms) %>%
+  dplyr::filter(!is.na(genus_correct)) %>%
+  dplyr::mutate('old name' = paste(family, genus, species, sep = " ")) %>%
+  dplyr::mutate('new name' = paste(family_correct, genus_correct, species_correct, sep = " ")) %>%
+  dplyr::select('old name', 'new name') %>%
+  dplyr::distinct()
+  
+
+length_upload <- length %>% # This includes only EM data, not generic length
+  mutate(family = if_else(genus %in% "Anoplocapros", "Aracanidae", family)) %>%
+  mutate(family = if_else(genus %in% "Aracana", "Aracanidae", family)) %>% 
+  mutate(genus = if_else(genus %in% "Dasyatis", "Bathytoshia", genus)) %>%
+  #mutate(genus = if_else(genus %in% "Dasyatis", "Bathytoshia", genus)) %>% #turn this on if you need it 
+  dplyr::left_join(., CheckEM::aus_synonyms) %>%
+  dplyr::mutate(genus = ifelse(!genus_correct%in%c(NA), genus_correct, genus)) %>%
+  dplyr::mutate(species = ifelse(!is.na(species_correct), species_correct, species)) %>%
+  dplyr::mutate(family = ifelse(!is.na(family_correct), family_correct, family)) %>%
+  dplyr::select(-c(family_correct, genus_correct, species_correct)) %>%
+  dplyr::mutate(scientific = paste(family, genus, species)) %>% # turn this on if you need it 
+  left_join(codes) %>%
+  dplyr::rename(rms_mm = rms, range_mm = range, precision_mm = precision, count = number) %>%
+  dplyr::select(-period) %>%
+  # filter(!is.na(caab_code)) %>%
+  select(campaignid, opcode, sample, caab_code, count, family, genus, species, length_mm, precision_mm, range_mm, rms_mm, stage)
+
+names(length_upload) %>% sort()
+
+# Check missing caab codes (okay if from sp, sp1 etc.)
+n_no_caab_count <- length(count_upload$species[which(is.na(count_upload$caab_code))])
+sp_no_caab_count <- unique(count_upload$species[which(is.na(count_upload$caab_code))])
+n_no_caab_length <- length(length_upload$species[which(is.na(length_upload$caab_code))])
+sp_no_caab_length <- unique(length_upload$species[which(is.na(length_upload$caab_code))])
+
+species_2_update <- count_upload %>%
+  filter(is.na(caab_code)) %>%
+  distinct(campaignid, opcode, family, genus, species, caab_code)
+
+# Save GA upload data ----
+write_csv(count_upload, paste0("data/uploads/", name, "_count.csv"))
+write_csv(length_upload, paste0("data/uploads/", name, "_length.csv"))
